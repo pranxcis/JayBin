120499457ab6daac9c50746632721619e8a0d929
jufranx <francisbalbin3@gmail.com>
2026-05-20 01:28:18 +0800

    Path

--- includes/projects.js
+++ includes/projects.js
@@ -9,7 +9,7 @@ const projects = [
 
     category: "Render • 3D Model • Blending • ITCI231-LAB-T",
 
-    image: "../resources/Solar System.png",
+    image: "resources/Solar System.png",
 
     description:
         "The Solar System project is a multimedia rendering study focused on recreating the planets of the solar system through 3D modeling and animation. Each planet was modeled and arranged in Blender 5.1.1 with the help of Python scripting for positioning and scene setup. The project explored frame-by-frame rendering, orbital movement simulation, lighting, and cinematic composition while improving my understanding of 3D modeling, animation, and rendering workflows.",
@@ -26,7 +26,7 @@ const projects = [
 
     category: "Frames • Animation • Tracing • ITCI231-LAB-T",
 
-    image: "../resources/Rotoscope.png",
+    image: "resources/Rotoscope.png",
 
     description:
         "The Rotoscope project is a frame-by-frame animation based on a real dance video. The footage was separated into image sequences, and each frame was manually traced using rotoscope tools to recreate movement and expression. Developed over 14 weeks, the project focused on timing, motion accuracy, and visual consistency while improving my understanding of animation and motion analysis.",
@@ -43,7 +43,7 @@ const projects = [
 
     category: "System • Case Study • Virtual Machine • ITOSYS-LAB-T",
 
-    image: "../resources/Placeholder.png",
+    image: "resources/Placeholder.png",
 
     description:
         "The Operating System project focuses on installing and exploring different operating systems using VirtualBox. The project involved setting up various OS environments, studying their installation processes, and performing case studies on their features, structure, and intended use. Through this project, I gained a better understanding of virtualization, system setup, and the practical use of operating systems.",
